e26d2499042457643a269126eb5841d8efaf753d
DeytonGarcia <136906803+DeytonGarcia@users.noreply.github.com>
2023-09-05 22:37:33 -0500

    Ejercicio

---
+++ Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg10_deytongarciaa_poo;
+
+public class Calculadora {
+    //Atributos
+    int a;
+    int b;
+    int mul;
+    //Metodo
+    public void multiplicar(){
+    mul = a * b;
+    System.out.println(mul);  
+    
+    }
+
+}
